54659473bc0e8f65db927ded4a2b1c7f64901985
AlexandreAlmeid <alexandreruizdealmeida@gmail.com>
2024-05-29 22:50:40 +0100

    MyGenericClass

---
+++ MyGenericClass/Guarda3.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace MyGenericClass
+{
+    public class Guarda3<T>
+    {
+        private T Var1;
+        private T Var2;
+        private T Var3;
+
+        public Guarda3()
+        {
+            Var1 = default;
+            Var2 = default;
+            Var3 = default;
+        }
+        public T GetItem(int i)
+        {
+            if (i < 0 || i > 2) throw new IndexOutOfRangeException();
+            else if (i == 0) return Var1;
+            else if (i == 1) return Var2;
+            else return Var3;
+        }
+
+        public void SetItem(int i, T item)
+        {
+            if (i < 0 || i > 2) throw new IndexOutOfRangeException();
+            else if (i == 0) Var1 = item;
+            else if (i == 1) Var2 = item;
+            else Var3 = item;
+        }
+    }
+}
